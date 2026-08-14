4f6b56f2d96f8fefb39d0b831045d49b67983bc6
Rana Gaber <158029182+ranagaber@users.noreply.github.com>
2026-08-14 23:46:56 +0300

    Optimize model usage with device allocation and tensor handling
    Moved model to the appropriate device and updated input handling for generation.

--- src/g2p/app.py
+++ src/g2p/app.py
@@ -1,13 +1,17 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch 
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 model_id = 'RanaGaber/G2P_DA_Large_country_level'
 
-model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id).to(device)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model.eval()
 
 def get_phoneme(text: str):
-    inputs = tokenizer(text , truncation = True)
-    outputs = model(**inputs)
+    inputs = tokenizer(text , truncation = True , return_tensors = "pt").to(device)
+    with torch.no_grad():
+        outputs = model.generate(**inputs)
     pred = tokenizer.decode(outputs[0] , skip_special_tokens = True)
     return pred
     
